809b5bbba1a6d87a070d08a7cf292ca983fce067
Paul Kourany <pkourany@users.noreply.github.com>
2015-09-17 08:49:51 -0400

    Add Photon compatibility

--- firmware/examples/PulseSensorAmped_Arduino_1dot4.ino
+++ firmware/examples/PulseSensorAmped_Arduino_1dot4.ino
@@ -1,11 +1,13 @@
 /*
->> Pulse Sensor Amped 1.2 <<
+>> Pulse Sensor Amped 1.4 <<
 This code is for Pulse Sensor Amped by Joel Murphy and Yury Gitman
 	www.pulsesensor.com 
-	>>> Pulse Sensor purple wire goes to Analog Pin A2 (see Interrupt.h for details) <<<
+	>>> Pulse Sensor purple wire goes to Analog Pin A2 (see PulseSensor_Spark.h for details) <<<
 	
-Pulse Sensor sample aquisition and processing happens in the background via a hardware Timer interrupt. 2mS sample rate.
-PWM on selectable pins A0 and A1 will not work when using this code, because the first allocated timer is TIMR2!
+Pulse Sensor sample aquisition and processing happens in the background via a hardware Timer interrupt at 2mS sample rate.
+On the Core, PWM on selectable pins A0 and A1 will not work when using this code, because the first allocated timer is TIMR2!
+On the Photon, TIMR3 is allocated and has no affect on the A2 pin.
+
 The following variables are automatically updated:
 Signal :    int that holds the analog signal data straight from the sensor. updated every 2mS.
 IBI  :      int that holds the time interval between beats. 2mS resolution.
@@ -17,7 +19,7 @@ This code is designed with output serial data to Processing sketch "PulseSensorA
 The Processing sketch is a simple data visualizer. 
 All the work to find the heartbeat and determine the heartrate happens in the code below.
 Pin D7 LED (onboard LED) will blink with heartbeat.
-If you want to use pin D7 for something else, specifiy different pin in Interrupt.h
+If you want to use pin D7 for something else, specifiy different pin in PulseSensor_Spark.h
 It will also fade an LED on pin fadePin with every beat. Put an LED and series resistor from fadePin to GND.
 Check here for detailed code walkthrough:
 http://pulsesensor.myshopify.com/pages/pulse-sensor-amped-arduino-v1dot1
@@ -26,6 +28,7 @@ Code Version 1.2 by Joel Murphy & Yury Gitman  Spring 2013
 This update fixes the firstBeat and secondBeat flag usage so that realistic BPM is reported.
 
 >>> Adapted for Spark Core by Paul Kourany, May 2014 <<<
+>>> Updated for Particle Core and Photon by Paul Kourany, Sept 2015 <<<
 
 */
 
@@ -59,9 +62,6 @@ void setup(){
 	pinMode(fadePin,OUTPUT);          // pin that will fade to your heartbeat!
 	Serial.begin(115200);             // we agree to talk fast!
 	interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS 
-	// UN-COMMENT THE NEXT LINE IF YOU ARE POWERING The Pulse Sensor AT LOW VOLTAGE, 
-	// AND APPLY THAT VOLTAGE TO THE A-REF PIN
-	//analogReference(EXTERNAL);   
 }
 
 
